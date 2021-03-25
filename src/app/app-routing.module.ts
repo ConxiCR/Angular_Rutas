cc2303e112fd3c4c9e79a55975b2bf00f38f5fc5
ConxiCr <conxicr@hotmail.com>
2021-03-25 12:52:45 +0100

    definir path & componet
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { InfoComponent } from './componentes/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'informacion', component: InfoComponent},
+  {path: 'sobre', component: AboutComponent},
+  {path: 'contacto', component: ContactComponent},
+  //redirigir una ruta
+  {path: 'about', redirectTo: 'sobre'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
