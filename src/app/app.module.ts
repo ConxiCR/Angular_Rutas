ad98224b537f06cd640188ae860d2da89e9b5ab0
ConxiCr <conxicr@hotmail.com>
2021-03-25 12:29:15 +0100

    creando rutas del rounting
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './componentes/info/info.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
